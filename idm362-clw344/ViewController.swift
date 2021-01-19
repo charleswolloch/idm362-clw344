c6766c01d5818de5e33293f21ae7147cfba7c725
Charles Wolloch <clw344@drexel.edu>
2021-01-19 09:50:43 -0500

    Initial Commit

---
+++ idm362-clw344/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idm362-clw344
+//
+//  Created by Charles Wolloch on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ idm362-clw344Tests/idm362_clw344Tests.swift
@@ -0,0 +1,33 @@
+//
+//  idm362_clw344Tests.swift
+//  idm362-clw344Tests
+//
+//  Created by Charles Wolloch on 1/19/21.
+//
+
+import XCTest
+@testable import idm362_clw344
+
+class idm362_clw344Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
